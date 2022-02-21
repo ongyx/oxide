991d9b7cd585e465228f461d7d88c61e4df44397
Ong Yong Xin <ongyongxin2020+github@gmail.com>
2022-02-21 23:12:50 +0800

    misc: cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -10,19 +10,7 @@ pub mod ast {
 }
 
 pub mod runtime {
-    pub mod types {
-        mod boolean;
-        mod float;
-        mod integer;
-        mod nil;
-        mod object;
-        mod string;
+    mod object;
 
-        pub use boolean::Boolean;
-        pub use float::Float;
-        pub use integer::Integer;
-        pub use nil::Nil;
-        pub use object::{Object, Type};
-        pub use string::Str;
-    }
+    pub use object::Object;
 }
